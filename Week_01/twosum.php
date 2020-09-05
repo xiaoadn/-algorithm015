b20dad0a4de4731697cdd666ae0839ad619e3351
李小茂 <304244853@qq.com>
2020-09-05 17:28:09 +0800

    两数之和

---
+++ Week_01/twosum.php
@@ -0,0 +1,34 @@
+<?php 
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $map = [];
+        $rst = [];
+        for($i=0; $i < count($nums); $i++) {
+            $val = $target - $nums[$i];
+            //echo '数字：：' . $val . "\n";
+            if (isset($map[$val])) {
+                echo $map[$val] . "\n";
+                return [$map[$val], $i];
+            }
+
+            //echo $i . "\n";
+
+            $map[$nums[$i]] = $i;
+        }
+        return $rst;
+    }
+}
+
+$solution = new Solution();
+$testNums = [2, 7, 11, 15];
+$target = 9;
+$rst = $solution->twoSum($testNums, $target);
+print_r($rst);
+
+// 2020-09-05
