aa08ebf179a83ec9fa2809a04ea16ab05551da1f
李小茂 <304244853@qq.com>
2020-09-10 23:42:59 +0800

    二叉树的最大深度

---
+++ Week_03/maximum-depth-of-binary-tree.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer
+     */
+    function maxDepth($root) {
+        if ($root === null) return 0;
+        $left = $this->maxDepth($root->left);
+        $right = $this->maxDepth($root->right);
+        return max($left, $right) + 1;
+    }
+}
+
+// 2020-09-10 leetcode中文
