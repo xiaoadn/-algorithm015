c4d6f3165ea4f92df61d2587256724c3935a47b0
李小茂 <304244853@qq.com>
2020-09-13 23:02:26 +0800

    数组子集

---
+++ Week_03/subsets.php
@@ -0,0 +1,33 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function subsets($nums) {
+        if (!$nums) return [];
+
+        $rst = [];
+        $this->helper($nums, 0, [], $rst);
+        return $rst;
+    }
+
+    function helper($nums, $index, $current, &$result)
+    {
+        // terminator
+        if ($index == count($nums)) {
+            $result[] = $current;
+            return $result;
+        }
+
+        // prepare datas
+        $this->helper($nums, $index+1, $current, $result);
+        // 
+        $current[] = $nums[$index];
+        $this->helper($nums, $index+1, $current, $result);
+    }
+
+}
+
+// 2020-09-13
