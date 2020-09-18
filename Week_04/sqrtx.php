72f047d0a0aff1e36deaea09d4a66ba5600886bd
李小茂 <304244853@qq.com>
2020-09-18 11:09:30 +0800

    fix

--- Week_04/sqrtx.php
+++ Week_04/sqrtx.php
@@ -6,36 +6,6 @@ class Solution {
      * @param Integer $x
      * @return Integer
      */
-    function mySqrt1($x) {
-        $left = 0;
-        $right = $x / 2;
-        
-        $mid = 0;
-        while ($left <= $right) {
-            $mid = ($left + $right) / 2 + 1;
-            $mid2 = $mid * $mid;
-            if ($mid2 == $x) {
-                break;
-            } elseif ($mid2 < $x) {
-                $left = $mid + 1;
-            } else {
-                $right = $mid -1;
-            }
-        }
-
-        return $mid;
-    }
-
-    function mySqrt3($x) 
-    {
-        $left = 0;
-        $right = $x;
-        $mid = 0;
-        while($left <= $right) {
-            $mid = ($right - $left) / 2 + $left;
-        }
-    }
-
     function mySqrt($x) 
     {
         $left = 0;
@@ -61,25 +31,6 @@ class Solution {
         return intval($ans);
     }
 
-
-    function mySqrt2($x) 
-    {
-        $left = 0;
-        $right = $x;
-        $mid = 0;
-        while($left <= $right) {
-            $mid = ($right - $left) / 2 + $left;
-            if (intval($mid * $mid) == $x) {
-                return intval($mid);
-            } elseif (intval($mid * $mid) > $x) {
-                $right = $mid;
-            } else {
-                $left = $mid;
-            }
-        }
-
-        return -1;
-    }
 }
 
 $solution = new Solution();
